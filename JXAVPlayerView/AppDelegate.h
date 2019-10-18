c3f77ff8381a51c67dc37b18d5007355b66658ca
chenzhe <chenzhe@baijiayun.com>
2019-10-18 17:21:52 +0800

    Initial Commit

---
+++ JXAVPlayerView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JXAVPlayerView
+//
+//  Created by JosephXuan on 2019/10/18.
+//  Copyright © 2019 JosephXuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JXAVPlayerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXAVPlayerView
+//
+//  Created by JosephXuan on 2019/10/18.
+//  Copyright © 2019 JosephXuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
